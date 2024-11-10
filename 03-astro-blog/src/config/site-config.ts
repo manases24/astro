9c03fccee6baf6a8f03d2b1e2a20e8522daf8e6b
manases24 <manases2030@gmail.com>
2024-11-09 21:11:30 -0300

    files are updated

--- 03-astro-blog/src/config/site-config.ts
+++ 03-astro-blog/src/config/site-config.ts
@@ -1,4 +1,4 @@
 export const siteConfig = {
-  title: "Fernando's Blog",
-  description: "Blog oficial de Fernando Herrera - Web developer",
+  title: "Evan You Blog",
+  description: "Web developer",
 };
