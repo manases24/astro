8c8918876f0cd9a0ca53302623931730df2453c7
manases24 <manases2030@gmail.com>
2024-11-10 16:49:59 -0300

    rss feed

---
+++ 05-rss-feed/astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), tailwind()],
+  site: "https://fernando-blog.netlify.app",
+});
