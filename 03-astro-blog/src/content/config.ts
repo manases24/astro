9227045f807a74ad9b274086ba6d42e080263463
manases24 <manases2030@gmail.com>
2024-11-10 14:50:10 -0300

    files are updated

--- 03-astro-blog/src/content/config.ts
+++ 03-astro-blog/src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
@@ -12,13 +12,29 @@ const blogCollection = defineCollection({
       }),
 
       // Relación
-      author: z.string(),
+      // author: z.string(),
+      author: reference("author"),
 
       // Relación
       tags: z.array(z.string()),
     }),
 });
 
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+      // twitter: z.string(),
+      // linkedIn: z.string(),
+      // github: z.string(),
+      // bio: z.string(),
+      // subtitle: z.string(),
+    }),
+});
+
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
